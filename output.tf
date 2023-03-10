d3b5722ab4e983eb5b59e36c8e0c58452583fa1b
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-03-10 13:57:26 +0100

    docs: add documentation

--- output.tf
+++ output.tf
@@ -12,4 +12,5 @@ output "oidc" {
 output "devops_stack_admins" {
   description = "Map containing the usernames and e-mails of the created users from `var.user_map`."
   value       = var.user_map != {} ? { for key, value in var.user_map : value.username => value.email } : {}
+  sensitive   = true
 }
